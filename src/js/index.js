a406164afb23426fa17f7ac030d3614b02fe5b1f
Alex Geic <tsindalex@mail.ru>
2019-06-04 18:06:35 +0300

    styles, parsing and rendering of data from json and fixed bags

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,40 @@
+$(document).ready(function() {
+  $("#slider").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: true
+  });
+});
+
 const xmr = new XMLHttpRequest();
-xmr.onreadystatechange = function () {
-    if(this.readyState == 4 && this.status == 200) {
-        var features = JSON.parse(this.responseText);
-        console.log(features[0]);
-        for(key in features) {
-            let titles = document.querySelectorAll('.features__img img');
-            titles[0].setAttribute("src", features[0].img);
-            console.log(titles[0].getAttribute("src"));
-            // titles.innerHTML = features[0].img;
-            console.log(titles);
-        }
-    }
+xmr.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var features = JSON.parse(this.responseText);
+    console.log(features);
+    features.forEach(value => {
+      let featuresData = document.getElementById("features");
+      let output =
+        "<div>" +
+        '<div class="features__img">' +
+        "<img " +
+        'src="' +
+        value.img +
+        '" ' +
+        'alt=""' +
+        "/>" +
+        "</div>" +
+        '<h4 class="features__title">' +
+        value.title +
+        "</h4>" +
+        '<p class="features__description">' +
+        value.description +
+        "</p>" +
+        "</div>";
+      featuresData.innerHTML += output;
+      console.log(featuresData);
+    });
+  }
 };
 xmr.open("GET", "/section-2.json", true);
 xmr.send();
