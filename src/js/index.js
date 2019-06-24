4ad3a95a9fd8d13ad3bd5c9143979fa9fb283101
Alex Geic <tsindalex@mail.ru>
2019-06-25 02:53:14 +0300

    rebuild project and add get response from server on net

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
     dots: true
   });
 });
-
-const xmr = new XMLHttpRequest();
-xmr.onreadystatechange = function() {
+// FOR LOCAL JSON FILE
+/* const xmr = new XMLHttpRequest();
+xmr.onload = function() {
   if (this.readyState == 4 && this.status == 200) {
     var features = JSON.parse(this.responseText);
     console.log(features);
     features.forEach(value => {
       let featuresData = document.getElementById("features");
-      let output =
+      let template =
         "<div>" +
         '<div class="features__img">' +
         "<img " +
@@ -31,7 +31,7 @@ xmr.onreadystatechange = function() {
         value.description +
         "</p>" +
         "</div>";
-      featuresData.innerHTML += output;
+      featuresData.innerHTML += template;
       // console.log(featuresData);
     });
   }
@@ -44,5 +44,44 @@ xmr.onreadystatechange = function() {
 // );
 //  LOCAL VERSION
 xmr.open("GET", "/section-2.json");
-xmr.send();
+xmr.send(); */
 // **************************************
+
+let findOutMore = document.getElementById("find-out");
+
+findOutMore.addEventListener("click", function() {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var jsonData = JSON.parse(this.responseText);
+      let featuresData = document.getElementById("features");
+      // let featuresImg = featuresData.querySelectorAll("img");
+      // let featuresTitle = document.querySelectorAll(".features__title");
+      let featuresImg = Array.prototype.slice.call(
+        featuresData.querySelectorAll("img"),
+        0
+      );
+      let featuresTitle = Array.prototype.slice.call(
+        featuresData.querySelectorAll(".features__title"),
+        0
+      );
+
+      let count = 0;
+      for (elem of jsonData) {
+        count++;
+        if (count === elem.id) {
+          featuresImg[count - 1].src = elem.thumbnailUrl;
+          featuresTitle[count - 1].innerHTML = elem.title;
+        }
+        if (count === 6) {
+          break;
+        }
+      }
+    } else {
+      console.error("Ошибка получения данных: " + xhr.error);
+    }
+  };
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/photos");
+  xhr.send();
+});
+/* ************************************************* */
